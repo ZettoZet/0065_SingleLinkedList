fab842ecea548bac2fd66c8305739a9a52b297c9
Pillow <m.farid.ft20@mail.umy.ac.id>
2025-05-08 19:20:22 +0700

    menambahkan variabel nim yang berfungsi untuk menampung data nim mahasiswa yang dimasukkan melalui input, lalu dipindahkan kedalam noMhs

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -21,7 +21,14 @@ public:
 
     void addNode()
     {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
 
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        
     }
 };
 
